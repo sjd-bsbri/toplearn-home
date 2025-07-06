ab90ed26bb6511b124ec63e4f75c9049ba1fa005
sajad <abccnn09@gmail.com>
2025-07-06 10:32:22 +0330

    change base name in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/toplearnHomePage",
+  base: "/toplearn-home",
 
   resolve: {
     alias: {
