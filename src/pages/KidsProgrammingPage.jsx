98d60f8d3b8d335e7bfdcb562890676dac4347a9
sajad <abccnn09@gmail.com>
2025-07-12 18:00:30 +0330

    add ProgrammingAndWebDesignPage and KidsProgrammingPage and IntroToProgrammingPage and DeafCoursesPage components

--- src/pages/KidsProgrammingPage.jsx
+++ src/pages/KidsProgrammingPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { FaChild, FaRobot, FaPuzzlePiece, FaArrowRight } from 'react-icons/fa';
